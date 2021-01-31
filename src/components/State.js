baa6328524a8cacb0abf0e67d313b9c3f3e7ea89
Patrick Cook <ptcook74@gmail.com>
2021-01-31 13:18:46 -0500

    message

---
+++ src/components/State.js
@@ -0,0 +1,20 @@
+//JS object wherre property values of the component are StereoPannerNode
+//Managed inside of the component instead of being passed into it
+//(like props)
+
+import { useState }  from 'react';
+
+const State = () => {
+    
+    return (
+        <>
+        <button>-</button>
+        <span> 0 </span>
+        <button>+</button>
+        </>
+    )
+}
+
+function withdraw()
+
+export default State;
