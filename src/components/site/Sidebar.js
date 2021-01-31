4e77c7fc2b60b4833b10d5a7f33d735884994564
Patrick Cook <ptcook74@gmail.com>
2021-01-31 17:35:35 -0500

    message

--- src/components/site/Sidebar.js
+++ src/components/site/Sidebar.js
@@ -9,6 +9,7 @@ import {
 import Home from './Home'
 import Resources from './Resources'
 import FunctionalComponentDemo from '../concepts/FunctionalComponentDemo'
+import JSXRules from '../site/JSXRules'
 
 
 const Sidebar = () => {
@@ -18,6 +19,7 @@ const Sidebar = () => {
                 <ul className="sidebar-list list-unstyled">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/functionalcomponent">Functional Component</Link></li>
+                    <li><Link to="/JSXRules">JSX Rules</Link></li>
                     <li><Link to="/resources">Resources</Link></li>
                 </ul>
             </div>
@@ -26,6 +28,7 @@ const Sidebar = () => {
                     <Route exact path="/home"><Home /></Route>
                     <Route exact path="/resources"><Resources /></Route>
                     <Route exact path="/"><Home /></Route>
+                    <Route exact path="/jsxrules"><JSXRules /></Route>
                     <Route exact path="/functionalcomponent"><FunctionalComponentDemo /></Route>
                 </Switch>
             </div>
